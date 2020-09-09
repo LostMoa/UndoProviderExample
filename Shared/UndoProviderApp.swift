39f1506b7c5ce108d6430262cdbbc1857f9a009c
Matthaus Woolard <matthaus.woolard@gmail.com>
2020-09-09 17:42:49 +1200

    Initial Commit

---
+++ Shared/UndoProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UndoProviderApp.swift
+//  Shared
+//
+//  Created by Matthaus Woolard on 09/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct UndoProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
